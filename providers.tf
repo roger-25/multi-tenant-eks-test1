071555c930824125ed796ce6be72d332dbce3eff
roger-25 <devopsroger@gmail.com>
2025-04-21 11:32:30 +0530

    test20

--- providers.tf
+++ providers.tf
@@ -1,13 +1,10 @@
 # providers.tf
 provider "aws" {
   region = "us-east-1"  # Explicit region declaration
-  # Add these if using specific credentials
-  # access_key = var.aws_access_key
-  # secret_key = var.aws_secret_key
 }
 
 data "aws_eks_cluster" "existing" {
-  name = "eksdemo1"
+  name = "terraform-eks-test"
 }
 
 data "aws_eks_cluster_auth" "cluster" {
