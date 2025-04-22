e6f366187ab7f584bfe617204b36915d0028e7a8
roger-25 <devopsroger@gmail.com>
2025-04-22 11:48:28 +0530

    test33

--- Cluster-ipservice.tf
+++ Cluster-ipservice.tf
@@ -8,13 +8,15 @@ resource "kubernetes_service_v1" "myapp1_service" {
   }
 
   spec {
-    type = "ClusterIP"  # Suitable for Ingress
+    type = "LoadBalancer"  # Change this to "LoadBalancer" to expose via ALB
+
     selector = {
       app = "myapp1"  # Matches your Deployment's pod labels
     }
+
     port {
       port        = 80    # Service port
-      target_port = 80    # Container port (updated to match pod)
+      target_port = 80    # Container port (should match your container's exposed port)
     }
   }
 }
