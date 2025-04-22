7a66d6566198a3ff2936a39c887494d9bb1e27a9
roger-25 <devopsroger@gmail.com>
2025-04-22 16:00:40 +0530

    test47

--- tags.tf
+++ tags.tf
@@ -1,12 +1,12 @@
 resource "aws_ec2_tag" "internal_elb_role" {
-  for_each    = toset(var.private_subnet_ids)
+  for_each    = toset(var.alb_public_subnet_ids)
   resource_id = each.key
   key         = "kubernetes.io/role/internal-elb"
   value       = "1"
 }
 
 resource "aws_ec2_tag" "eks_cluster_tag" {
-  for_each    = toset(var.private_subnet_ids)
+  for_each    = toset(var.alb_public_subnet_ids)
   resource_id = each.key
   key         = "kubernetes.io/cluster/${var.cluster_name}"
   value       = "owned"
