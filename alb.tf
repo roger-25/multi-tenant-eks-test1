3fe69769ddc871c08a7a4be65b767d0d4ba78564
roger-25 <devopsroger@gmail.com>
2025-04-23 11:42:35 +0530

    test49

--- alb.tf
+++ alb.tf
@@ -2,12 +2,11 @@ resource "aws_lb" "tenant_alb" {
   name               = "alb-${var.tenant_name}"
   internal           = false
   load_balancer_type = "application"
-  subnets            = var.alb_public_subnet_ids
+  subnets            = var.public_subnet_ids
   security_groups    = [var.security_group_id]
 
   tags = {
     Name    = "alb-${var.tenant_name}"
     Tenant  = var.tenant_name
-    Env     = var.environment
   }
 }
